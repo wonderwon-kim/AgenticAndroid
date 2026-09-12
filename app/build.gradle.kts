d654ae09401896ffb6c18c798adaab15324d60ee
wonderwon <davidbackhome1004@gmail.com>
2026-09-12 07:14:39 +0000

    release: version production automation as v2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agenticandroid.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
