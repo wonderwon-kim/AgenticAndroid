9f2fc67bee931ef38460cb8c58643d65baf7334e
wonderwon <davidbackhome1004@gmail.com>
2026-09-12 06:44:28 +0000

    fix: isolate minimal solar package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            applicationIdSuffix = ".solar"
+            versionNameSuffix = "-solar"
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
