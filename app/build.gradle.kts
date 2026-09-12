9bfd1dc5761524ddd5c62b720c19743a413d44d8
wonderwon <davidbackhome1004@gmail.com>
2026-09-12 06:44:31 +0000

    fix: isolate futuristic package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            applicationIdSuffix = ".futuristic"
+            versionNameSuffix = "-futuristic"
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
